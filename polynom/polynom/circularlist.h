3e89e07fd4bfe59d9c748538973d00ff0d32f984
Alexander Gaivanuk <alexgaiv0748@gmail.com>
2016-03-02 10:04:21 +0300

    polynom lab

---
+++ polynom/polynom/circularlist.h
@@ -0,0 +1,10 @@
+#ifndef _CIRCULAR_LIST_H_
+#define _CIRCULAR_LIST_H_
+
+template<class T>
+class CircularList
+{
+public:
+}
+
+#endif // _CIRCULAR_LIST_H_
---
+++ polynom/polynom/monom.h
@@ -0,0 +1,11 @@
+#ifndef _MONOM_H_
+#define _MONOM_H_
+
+class Monom
+{
+public:
+	int coeff;
+	int conv;
+};
+
+#endif // _MONOM_H_
